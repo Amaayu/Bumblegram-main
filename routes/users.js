11f6f3183af792b475550c4c73fdb1cbc01068c1
Amaayu <namdevaniket446@gmail.com>
2023-12-18 23:53:02 +0530

    api_kiy_issu

--- routes/users.js
+++ routes/users.js
@@ -8,9 +8,10 @@ const upload = require("./multer");
 const session = require("express-session");
 const cloudinary = require("cloudinary").v2;
 require("dotenv").config();
+// api_key: process.env.CLOUDINARY_API_KEY
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
+  api_key: "412351391672618",
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
