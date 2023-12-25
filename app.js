700b8238e55dee8929a4c5676d0987101577c5a2
Amaayu <namdevaniket446@gmail.com>
2023-12-25 19:07:34 +0530

    Port changes

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-const PORT =3000;
+const PORT =process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
